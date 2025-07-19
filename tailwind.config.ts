58871d9ab0716673fc5a2256989552ac6e8669a9
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-07-19 10:09:06 -0700

    Fix xs breakpoint and slot container alignment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '475px',
+      },
       fontFamily: {
         casino: ['"Casino"'],
         k: ['"Korean"'],

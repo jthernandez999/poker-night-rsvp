f472ed1c209b53339ab11848261247a887fec677
Joe Hernandez <j.t.hernandez999@gmail.com>
2025-07-19 13:13:17 -0700

    Update casino header with golden theme: handwriting font, golden neon text, and golden light bulbs with clockwise pulse animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,66 +6,66 @@ export default function Header() {
       <div className="w-full text-center">
         <div className="casino-sign inline-block relative w-full">
           {/* Light Bulbs Around the Border - Evenly Spaced */}
-          {/* Top Row - 19 bulbs evenly spaced (avoiding corner overlaps) */}
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '2%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '7.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '13%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '18.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '24%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '29.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '35%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '40.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '46%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '51.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '57%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '62.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '68%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '73.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '79%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '84.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '90%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-top" style={{ top: '-8px', left: '95.5%', zIndex: 100 }}></div>
+          {/* Top Row - 19 bulbs evenly spaced (left to right for clockwise) */}
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0" style={{ top: '-8px', left: '2%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.04" style={{ top: '-8px', left: '7.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.08" style={{ top: '-8px', left: '13%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.12" style={{ top: '-8px', left: '18.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.16" style={{ top: '-8px', left: '24%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.2" style={{ top: '-8px', left: '29.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.24" style={{ top: '-8px', left: '35%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.28" style={{ top: '-8px', left: '40.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.32" style={{ top: '-8px', left: '46%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.36" style={{ top: '-8px', left: '51.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.4" style={{ top: '-8px', left: '57%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.44" style={{ top: '-8px', left: '62.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.48" style={{ top: '-8px', left: '68%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.52" style={{ top: '-8px', left: '73.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.56" style={{ top: '-8px', left: '79%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.6" style={{ top: '-8px', left: '84.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.64" style={{ top: '-8px', left: '90%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-top" data-pulse-delay="0.68" style={{ top: '-8px', left: '95.5%', zIndex: 100 }}></div>
           
-          {/* Right Column - 6 bulbs evenly spaced (avoiding corner overlaps) */}
-          <div className="light-bulb light-bulb-right" style={{ right: '-8px', top: '8%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-right" style={{ right: '-8px', top: '24%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-right" style={{ right: '-8px', top: '40%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-right" style={{ right: '-8px', top: '56%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-right" style={{ right: '-8px', top: '72%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-right" style={{ right: '-8px', top: '88%', zIndex: 100 }}></div>
+          {/* Right Column - 6 bulbs evenly spaced (top to bottom for clockwise) */}
+          <div className="light-bulb light-bulb-right" data-pulse-delay="0.72" style={{ right: '-8px', top: '8%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-right" data-pulse-delay="0.76" style={{ right: '-8px', top: '24%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-right" data-pulse-delay="0.8" style={{ right: '-8px', top: '40%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-right" data-pulse-delay="0.84" style={{ right: '-8px', top: '56%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-right" data-pulse-delay="0.88" style={{ right: '-8px', top: '72%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-right" data-pulse-delay="0.92" style={{ right: '-8px', top: '88%', zIndex: 100 }}></div>
           
-          {/* Bottom Row - 19 bulbs evenly spaced (right to left for clockwise, avoiding corner overlaps) */}
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '2%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '7.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '13%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '18.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '24%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '29.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '35%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '40.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '46%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '51.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '57%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '62.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '68%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '73.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '79%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '84.5%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '90%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-bottom" style={{ bottom: '-8px', left: '95.5%', zIndex: 100 }}></div>
+          {/* Bottom Row - 19 bulbs evenly spaced (right to left for clockwise) */}
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="0.96" style={{ bottom: '-8px', left: '95.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1" style={{ bottom: '-8px', left: '90%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.04" style={{ bottom: '-8px', left: '84.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.08" style={{ bottom: '-8px', left: '79%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.12" style={{ bottom: '-8px', left: '73.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.16" style={{ bottom: '-8px', left: '68%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.2" style={{ bottom: '-8px', left: '62.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.24" style={{ bottom: '-8px', left: '57%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.28" style={{ bottom: '-8px', left: '51.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.32" style={{ bottom: '-8px', left: '46%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.36" style={{ bottom: '-8px', left: '40.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.4" style={{ bottom: '-8px', left: '35%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.44" style={{ bottom: '-8px', left: '29.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.48" style={{ bottom: '-8px', left: '24%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.52" style={{ bottom: '-8px', left: '18.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.56" style={{ bottom: '-8px', left: '13%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.6" style={{ bottom: '-8px', left: '7.5%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-bottom" data-pulse-delay="1.64" style={{ bottom: '-8px', left: '2%', zIndex: 100 }}></div>
           
-          {/* Left Column - 6 bulbs evenly spaced (avoiding corner overlaps) */}
-          <div className="light-bulb light-bulb-left" style={{ left: '-8px', top: '8%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-left" style={{ left: '-8px', top: '24%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-left" style={{ left: '-8px', top: '40%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-left" style={{ left: '-8px', top: '56%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-left" style={{ left: '-8px', top: '72%', zIndex: 100 }}></div>
-          <div className="light-bulb light-bulb-left" style={{ left: '-8px', top: '88%', zIndex: 100 }}></div>
+          {/* Left Column - 6 bulbs evenly spaced (bottom to top for clockwise) */}
+          <div className="light-bulb light-bulb-left" data-pulse-delay="1.68" style={{ left: '-8px', top: '88%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-left" data-pulse-delay="1.72" style={{ left: '-8px', top: '72%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-left" data-pulse-delay="1.76" style={{ left: '-8px', top: '56%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-left" data-pulse-delay="1.8" style={{ left: '-8px', top: '40%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-left" data-pulse-delay="1.84" style={{ left: '-8px', top: '24%', zIndex: 100 }}></div>
+          <div className="light-bulb light-bulb-left" data-pulse-delay="1.88" style={{ left: '-8px', top: '8%', zIndex: 100 }}></div>
  
           
           {/* Title Inside the Sign */}
           <div className="w-full mb-2">
-            <h1 className="text-5xl md:text-7xl lg:text-9xl xl:text-[12rem] 2xl:text-[15rem] font-bold font-casino text-transparent bg-clip-text bg-gradient-to-br from-[#D2B688] to-[#7A3F33] w-full text-center">
+            <h1 className="text-5xl md:text-7xl lg:text-9xl xl:text-[12rem] 2xl:text-[15rem] font-bold font-handwriting text-white w-full text-center neon-text">
               <div className="w-full">Hernandez</div>
               <div className="w-full">Casino</div>
             </h1>
